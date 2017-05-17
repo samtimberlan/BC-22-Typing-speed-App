8ea659d842da3151ab099164165ea3f118478c46
Samtim <timudoma@gmail.com>
2017-05-17 14:53:53 +0100

    Add express

---
+++ server/config/routes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express(); 
+const path = require('path')
+
+app.use(express.static('public'));
+
+
+/* GET home page. */
+app.get('/', function(request, response, next) {
+    response.sendFile("index");
+});
+
+// GET typingSpeedPage
+app.get("/typingspeed", function (request,response) {
+        response.sendFile(path.resolve("public/typingSpeedPageMain.html"));
+    });
+
+    
+    app.listen(8080, function (err) {
+        if(err){
+            //GET 404 page
+    app.get("/typingspeed", function (request,response) {
+        response.sendFile(path.resolve("public/404.html"));
+    });
+        }
+    });
+
+module.exports = app;
