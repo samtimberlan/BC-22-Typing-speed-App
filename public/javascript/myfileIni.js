4d36be2ab59ccd72cf60ec13df4af87b1b6eb96d
Samtim <timudoma@gmail.com>
2017-05-18 22:34:44 +0100

    Update typingSpeed page

--- public/javascript/myfileIni.js
+++ public/javascript/myfileIni.js
@@ -1,4 +1,7 @@
-// Initialize Firebase
+
+<script src="https://www.gstatic.com/firebasejs/4.0.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
   var config = {
     apiKey: "AIzaSyBX21V-_yleAu482Dtzj3Ze8Y4_q_TS2F8",
     authDomain: "typingspeedapp.firebaseapp.com",
@@ -8,8 +11,4 @@
     messagingSenderId: "307724520627"
   };
   firebase.initializeApp(config);
-  
-  
-  
-  
-  
+</script>
--- public/javascript/typingSpeedPage.js
+++ public/javascript/typingSpeedPage.js
@@ -23,63 +23,65 @@ let timeInSecs = 60;  // Variable to represent 1 minute
  }*/
 
   
-const beginTest = () => {
-  timer = 
-   setInterval(()=>{
-      let timerDisplay = document.getElementById("displayInformation");
-      if(timeInSecs !== 0){
-        timeInSecs --;
-        if(timeInSecs >= 10){
-          timerDisplay.innerHTML = timeInSecs + "s remaining";
-          timerDisplay.style.color= "green";
+  const beginTest = () => {
+    timer = 
+    setInterval(()=>{
+        let timerDisplay = document.getElementById("displayInformation");
+        if(timeInSecs !== 0){
+          timeInSecs --;
+          if(timeInSecs >= 10){
+            timerDisplay.innerHTML = timeInSecs + "s remaining";
+            timerDisplay.style.color= "green";
+          }
+          else if(timeInSecs < 10){
+            timerDisplay.innerHTML = timeInSecs + 's '+"<p>Come on, You can do it!</p>";
+            timerDisplay.style.color= "red";
+          }
+          }else if(timeInSecs <= 0){
+            clearInterval(timer);
+            timerDisplay.innerHTML = timeInSecs + 's '+"<p><b>Time Up!<b></p>";
+            endTest();
         }
-        else if(timeInSecs < 10){
-          timerDisplay.innerHTML = timeInSecs + 's '+"<p>Come on, You can do it!</p>";
-          timerDisplay.style.color= "red";
-        }
-        }else if(timeInSecs <= 0){
-          clearInterval(timer);
-          timerDisplay.innerHTML = timeInSecs + 's '+"<p><b>Time Up!<b></p>";
-          endTest();
-      }
-},1000);      
-}
- document.querySelector("#startTest").addEventListener('click', beginTest);
- document.querySelector("#inputType").addEventListener('focus', beginTest);
-
-
-const endTest = ()=>{
-
-  clearInterval(timer);
-  let typedWord = document.getElementById('inputType').value;
-  document.getElementById('inputType').disabled = true;
-  let displayInformation = document.getElementById("displayInformation");
-
-  if(typedWord === ""){
-    displayInformation.innerHTML = "Hey it seems you did not type anything.";
+  },1000);      
   }
-  else{
-    document.getElementById('wordToTypeField').value;
-    let calcNetWPM = () => {
-      let orignalLettersArray = stringWord.split('');
-   let neededLettersArray = typedWord.split('');
-     for(counter = 0; counter < neededLettersArray.length; counter++){
-         if(neededLettersArray[counter] !== orignalLettersArray[counter]){
-             errorCount++;
-         }
+  document.querySelector("#startTest").addEventListener('click', beginTest);
+  document.querySelector("#inputType").addEventListener('focus', beginTest);
+
+/**
+ * Function to calculate typing speed
+ */
+  const endTest = ()=>{
+
+    clearInterval(timer);
+    let typedWord = document.getElementById('inputType').value;
+    document.getElementById('inputType').disabled = true;
+    let displayInformation = document.getElementById("displayInformation");
+
+    if(typedWord === ""){
+      displayInformation.innerHTML = "Hey it seems you did not type anything.";
     }
-    const result = ((neededLettersArray.length/5)-errorCount)/1;
-if(result > 0){
-return JSON.parse(`{ "result" : ${result}, "errors": ${errorCount}}`);
-}
- return 0;  
-}
-   
-    let typeSpeedInformation = "";
-    displayInformation.innerHTML =  "Your typing speed is: " + calcNetWPM().result + "WPM";
+    else{
+      document.getElementById('wordToTypeField').value;
+      let calcNetWPM = () => {
+        let orignalLettersArray = stringWord.split('');
+        let neededLettersArray = typedWord.split('');
+      for(counter = 0; counter < neededLettersArray.length; counter++){
+          if(neededLettersArray[counter] !== orignalLettersArray[counter]){
+              errorCount++;
+          }
+      }
+      const result = ((neededLettersArray.length/5)-errorCount)/1;
+  if(result > 0){
+  return JSON.parse(`{ "result" : ${result}, "errors": ${errorCount}}`);
   }
-}
+  return 0;  
+  }
+      let typeSpeedInformation = "";
+      displayInformation.innerHTML =  "Your typing speed is: " + calcNetWPM().result + "WPM"
+      + "<p>You had " + errorCount + " error(s).</p>";
+    }
+  }
 
 
- 
-document.querySelector("#stopTest").addEventListener('click', endTest);
+  
+  document.querySelector("#stopTest").addEventListener('click', endTest);
