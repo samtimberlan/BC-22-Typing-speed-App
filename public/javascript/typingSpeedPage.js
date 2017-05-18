0d688705c56b9a27c27337460f34607327023f81
Samtim <timudoma@gmail.com>
2017-05-19 00:04:00 +0100

    Update html. Add relevant icons

--- public/javascript/typingSpeedPage.js
+++ public/javascript/typingSpeedPage.js
@@ -1,15 +1,7 @@
 let stringWord = "Innovative Technical Solutions, LLC (ITS) is a Native American owned business that was established in Paducah, Kentucky in April 2000. ITS is a certified and Small Disadvantaged Business by the U.S. Small Business Administration.Our headquarters " + 
                 "are in Paducah, Kentucky and we have" + 
                 " satellite offices located in Tennessee, " + 
-                "Ohio, and Illinois. ITS is a leading" + 
-                " edge Information Technology firm that " + 
-                "is comprised of professionals with a broad" + 
-                " range of experience in software " + 
-                "development, high-speed imaging/scanning (TIFF, PDF, Text, " + 
-                "and OCR capabilities), document management, records management," + 
-                " relevance management, information security, environmental " + 
-                "management, fire services management, fire protection " + 
-                "engineering, and protective force expertise.";
+                "Ohio, and Illinois. "
                 
 let neededWord = document.getElementById('wordToTypeField');
 neededWord.innerHTML = stringWord;
