3856cd701d42c05be966277e7fae9ba9c59c2937
Samtim <timudoma@gmail.com>
2017-05-18 21:28:41 +0100

    Update code logic

--- public/javascript/typingSpeedPage.js
+++ public/javascript/typingSpeedPage.js
@@ -1,32 +1,29 @@
-let stringWord = "Innovative Technical Solutions, LLC (ITS) is a Native American owned business that was established in Paducah, Kentucky in April 2000. ITS is a certified and Small Disadvantaged Business by the U.S. Small Business Administration.";
+let stringWord = "Innovative Technical Solutions, LLC (ITS) is a Native American owned business that was established in Paducah, Kentucky in April 2000. ITS is a certified and Small Disadvantaged Business by the U.S. Small Business Administration.Our headquarters " + 
+                "are in Paducah, Kentucky and we have" + 
+                " satellite offices located in Tennessee, " + 
+                "Ohio, and Illinois. ITS is a leading" + 
+                " edge Information Technology firm that " + 
+                "is comprised of professionals with a broad" + 
+                " range of experience in software " + 
+                "development, high-speed imaging/scanning (TIFF, PDF, Text, " + 
+                "and OCR capabilities), document management, records management," + 
+                " relevance management, information security, environmental " + 
+                "management, fire services management, fire protection " + 
+                "engineering, and protective force expertise.";
+
+                
 let neededWord = document.getElementById('wordToTypeField');
 neededWord.innerHTML = stringWord;
 let errorCount = 0;
 let timer;
 let timeInSecs = 60;  // Variable to represent 1 minute
 
-
+/* Uncomment to access 
  const calcGrossWPM = (wordsTyped) => {
      return Math.round((wordsTyped.length/5)/1);  // Divides by one or any time specified by the timer
- }
+ }*/
 
- const calcNetWPM = (originalWords, typedWords) => {
-   let orignalLettersArray = originalWords.split('');
-   let neededLettersArray = originalWords.split('');
-     for(counter = 0; counter < typedWords.length; counter++){
-         if(typedWords[counter] !== originalWords[counter]){
-             errorCount++;
-         }
- }
- const result = ((neededLettersArray.length/5)-errorCount)/1;
-if(result > 0){
-return JSON.parse(`{ "result" : ${result}, "errors": ${errorCount}}`);
-}
- return 0;  
-}
-
-
- 
+  
 const beginTest = () => {
   timer = 
    setInterval(()=>{
@@ -53,27 +50,37 @@ const beginTest = () => {
 
 
 const endTest = ()=>{
+
   clearInterval(timer);
   let typedWord = document.getElementById('inputType').value;
   document.getElementById('inputType').disabled = true;
   let displayInformation = document.getElementById("displayInformation");
+
   if(typedWord === ""){
     displayInformation.innerHTML = "Hey it seems you did not type anything.";
   }
   else{
-    console.log(typedWord);
-    let calcWpm = calcGrossWPM(typedWord);
-    let calNWpm = calcNetWPM(stringWord, typedWord);
-     if(calcNetWPM().result> 0){
-      let netResult = calcNetWPM().result;
-    }else{
-      let netResult = 0;
+    document.getElementById('wordToTypeField').value;
+    let calcNetWPM = () => {
+      let orignalLettersArray = stringWord.split('');
+   let neededLettersArray = typedWord.split('');
+     for(counter = 0; counter < neededLettersArray.length; counter++){
+         if(neededLettersArray[counter] !== orignalLettersArray[counter]){
+             errorCount++;
+         }
     }
-
+    const result = ((neededLettersArray.length/5)-errorCount)/1;
+if(result > 0){
+return JSON.parse(`{ "result" : ${result}, "errors": ${errorCount}}`);
+}
+ return 0;  
+}
+   
     let typeSpeedInformation = "";
-    displayInformation.innerHTML = calcGrossWPM;
+    displayInformation.innerHTML =  "Your typing speed is: " + calcNetWPM().result + "WPM";
   }
 }
+
 
  
 document.querySelector("#stopTest").addEventListener('click', endTest);
